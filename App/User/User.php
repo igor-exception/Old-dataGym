043c594682dca509b637dd5626d83bbca1617ec9
igor oliveira <igoroliveira.oliveira@gmail.com>
2022-12-22 06:05:12 -0300

    type added

--- App/User/User.php
+++ App/User/User.php
@@ -16,71 +16,64 @@ class User {
         $this->setPassword($password, $password_confirmation);
     }
 
-    private function setName($name)
+    private function setName($name): void
     {
         $name = htmlspecialchars($name);
         if(empty($name)){
             $this->addError("Nome não pode ser vazio.");
-            return false;
         }
 
         if(strlen($name) < 3){
             $this->addError("Nome precisa ser maior ou igual a 3 caracteres.");
-            return false;
         }
 
         if(strlen($name) > 256){
             $this->addError("Nome precisa ser menor ou igual a 256 caracteres.");
-            return false;
         }
 
         $this->name = $name;
     }
 
-    private function setEmail($email)
+    private function setEmail($email): void
     {
         $email = htmlspecialchars($email);
         
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
             $this->addError("Email no formato inválido.");
-            return false;
         }
 
         if(strlen($email) > 65) {
             $this->addError("Email deve ser menor ou igual a 65 caracteres.");
-            return false;
         }
 
         $this->email = $email;
     }
 
-    private function setPassword($password, $password_confirmation)
+    private function setPassword($password, $password_confirmation): void
     {
         if(strlen($password) < 8) {
             $this->addError("Senha deve ter no mínimo 8 caracteres.");
-            return false;
         }
 
         if($password !== $password_confirmation) {
             $this->addError("Senha e confirmação de senha devem ser iguais.");
-            return false;
         }
 
         $this->password = $password;
     }
 
 
-    private function addError($error_message)
+    private function addError($error_message): void
     {
         $this->errorMessages[] = $error_message;
     }
 
-    public function getErrorMessages()
+    public function getErrorMessages(): array
     {
         return $this->errorMessages;
     }
 
-    public function thereIsErrors()
+    public function thereIsErrors(): bool
     {
         return !empty($this->errorMessages);
     }
--- view/signup.php
+++ view/signup.php
@@ -7,15 +7,16 @@ if(isset($_POST['InputName'] , $_POST['InputEmail'], $_POST['InputPassword'], $_
 {
     $user = new \App\User\User($_POST['InputName'] , $_POST['InputEmail'], $_POST['InputPassword'], $_POST['InputPasswordConfirmation']);
 
-    if($user->thereIsErrors()){
-        foreach($user->getErrorMessages() as $error ){
-            echo "<p>{$error}</p>";
-        }
+    // se nao tiver erro, redireciona para home
+    if(!$user->thereIsErrors()){
+        header('Location: ../index.php?msg=success');
+        exit;
     }
 
-    header('Location: ../index.php?msg=success');
-    exit;
-
+    // mostra os erros
+    foreach($user->getErrorMessages() as $error ){
+        echo "<p>{$error}</p>";
+    }
 }
 
 require_once __DIR__ . './../includes/_signup.php';
