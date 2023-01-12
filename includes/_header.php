fcfa6e320f87a244b029192a17a805d52fa2b06a
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2023-01-11 21:57:52 -0300

    add .htaccess

--- includes/_header.php
+++ includes/_header.php
@@ -13,14 +13,14 @@
   <body>
       <?php session_start() ?>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="./../index.php">DataGymn</a>
+        <a class="navbar-brand" href="./../">DataGymn</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="./../index.php">Home</a>
+              <a class="nav-link" href="./../">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/view/signin.php">Entrar</a>
---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../includes/_header.php';
+
+if(isset($_GET['msg'])){
+    if ($_GET['msg'] == 'success') {
+        echo "<p>Cadastro realizado com sucesso!</p>";
+    }
+}
+?>
+
+<div class="row">
+    <div class="col">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Sobre</h5>
+                <p class="card-text">DataGym tem como objetivo registrar progresso de cargas e repetições nos exercícios.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Registrar/Logar</h5>
+                <p class="card-text">Caso já tenha conta, faça login. Ou, registre-se</p>
+                <a href="./../view/signin.php" class="card-link">Login</a>
+                <a href="./../view/signup.php" class="card-link">Nova conta</a>
+            </div>
+        </div>
+    </div>
+  </div>
+
+<?php
+require_once __DIR__ . '/../includes/_footer.php';
+?>
